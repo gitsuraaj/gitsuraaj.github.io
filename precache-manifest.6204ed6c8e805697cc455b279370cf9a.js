216b5a359fc034ee05496193d2b9f2dfe78ea6c9
Suraaj Hasija <mailbox.suraaj@gmail.com>
2023-03-14 05:51:38 +0530

    Updates

---
+++ precache-manifest.6204ed6c8e805697cc455b279370cf9a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "dddbbc33a82f1abecfa7",
+    "url": "/static/css/main.6982bd13.chunk.css"
+  },
+  {
+    "revision": "dddbbc33a82f1abecfa7",
+    "url": "/static/js/main.dddbbc33.chunk.js"
+  },
+  {
+    "revision": "5916edac1223a117cd3b",
+    "url": "/static/js/1.5916edac.chunk.js"
+  },
+  {
+    "revision": "d32f914b07985d0e9e12",
+    "url": "/static/css/2.89fba186.chunk.css"
+  },
+  {
+    "revision": "d32f914b07985d0e9e12",
+    "url": "/static/js/2.d32f914b.chunk.js"
+  },
+  {
+    "revision": "701c4e2b73a97a260a44",
+    "url": "/static/js/runtime~main.701c4e2b.js"
+  },
+  {
+    "revision": "b37b05a713d8eebcbb475119e587859b",
+    "url": "/static/media/github.b37b05a7.png"
+  },
+  {
+    "revision": "e00220b546d7b945d234fcb08dd2d808",
+    "url": "/static/media/gmail.e00220b5.png"
+  },
+  {
+    "revision": "fd0d5546fdbdc85c76c4372a0d51f1bc",
+    "url": "/static/media/linkedin.fd0d5546.png"
+  },
+  {
+    "revision": "8f5ce27564945d2c9a10ef827549a78c",
+    "url": "/static/media/facebook.8f5ce275.png"
+  },
+  {
+    "revision": "65307fbbc0d3435f76472f78d72fb97a",
+    "url": "/static/media/medium.65307fbb.png"
+  },
+  {
+    "revision": "b5e9703b22d2ae5f7df0358df6ac2b58",
+    "url": "/static/media/profile.b5e9703b.jpg"
+  },
+  {
+    "revision": "4a8c23476a7c20c5bee2a752a6f96e9e",
+    "url": "/static/media/instagram.4a8c2347.png"
+  },
+  {
+    "revision": "65e1947283353eb0cf96af33bb8b1220",
+    "url": "/static/media/6.65e19472.gif"
+  },
+  {
+    "revision": "8e3b3b9160de98c55be60ba6312ef067",
+    "url": "/static/media/fork.8e3b3b91.svg"
+  },
+  {
+    "revision": "d4a192e8220d39c29a25ee1be3ed7190",
+    "url": "/static/media/logo.d4a192e8.png"
+  },
+  {
+    "revision": "7a6f510e906361a65ec4137fd7fec116",
+    "url": "/static/media/5.7a6f510e.gif"
+  },
+  {
+    "revision": "375f838246043f2ea9d7f0a3d9b55275",
+    "url": "/index.html"
+  }
+];
